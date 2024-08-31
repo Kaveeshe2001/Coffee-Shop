a8f1618cea159d4cb8e04b81ef4277c74f894bda
Kaveesha <kaveeshewaduge111@gmail.com>
2024-08-31 16:17:39 +0530

    animation of wheretobuy

--- my-project/src/Components/WhereToBuy/WhereToBuy.jsx
+++ my-project/src/Components/WhereToBuy/WhereToBuy.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import WorldMap from '../../assets/world-map.png'
+import { motion } from 'framer-motion';
 
 const WhereToBuy = () => {
   return (
@@ -7,19 +8,55 @@ const WhereToBuy = () => {
       <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center'>
         {/* form section */}
         <div className='space-y-8'>
-            <h1 className='text-4xl font-bold text-darkGray font-serif'>Buy our products from anywhere</h1>
-            <div className='flex items-center gap-4'>
+            <motion.h1 
+            initial={{opacity: 0, y: 100}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+                delay: 0.2,
+            }}
+            className='text-4xl font-bold text-darkGray font-serif'>Buy our products from anywhere</motion.h1>
+            <motion.div
+            initial={{opacity: 0, y: 100}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+                delay: 0.6,
+            }}
+            className='flex items-center gap-4'>
                 <input type='text' placeholder='Name' className='input-style w-full lg:w-[150px]' />
                 <input type='email' placeholder='Email' className='input-style w-full' />
                 <input type='text' placeholder='Country' className='input-style w-full' />
                 <input type='text' placeholder='ZipCode' className='input-style w-full lg:w-[150px]' />
-            </div>
+            </motion.div>
 
-            <button className='primary-btn w-full'>Order Now</button>
+            <motion.button 
+            initial={{opacity: 0, y: 100}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{
+                delay: 0.8,
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+            }}
+            className='primary-btn w-full'>Order Now</motion.button>
         </div>
         {/* world map section */}
         <div className='col-span-2'>
-            <img src={WorldMap} alt='' className='w-full sm:w-[500px] mx-auto' />
+            <motion.img 
+            initial={{opacity: 0, scale:0.5}}
+            whileInView={{opacity: 1, scale:1}}
+            transition={{
+                delay: 1,
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+            }}
+            src={WorldMap} alt='' className='w-full sm:w-[500px] mx-auto' />
         </div>
       </div>
     </div>
