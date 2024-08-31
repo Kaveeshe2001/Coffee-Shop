e8c9d4755eff92dfcfcf694f7b437e80f7d19b3c
Kaveesha <kaveeshewaduge111@gmail.com>
2024-08-31 15:19:41 +0530

    world map of wheretobuy section

---
+++ my-project/src/Components/WhereToBuy/WhereToBuy.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import WorldMap from '../../assets/world-map.png'
+
+const WhereToBuy = () => {
+  return (
+    <div className='container my-36'>
+      <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center'>
+        {/* form section */}
+        <div></div>
+        {/* world map section */}
+        <div className='col-span-2'>
+            <img src={WorldMap} alt='' className='w-full sm:w-[500px] mx-auto' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WhereToBuy
